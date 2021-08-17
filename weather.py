07a5b35ac2e0d29ecf8a1910d9c963bf6a6e564c
Anand <87514488+httpanand@users.noreply.github.com>
2021-08-17 13:06:28 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request,abort
+import json
+import urllib.request
+
+
+app = Flask(__name__)
+def tocelcius(temp):
+    return str(round(float(temp) - 273.16,2))
+
+@app.route('/',methods=['POST','GET'])
+def weather():
+    api_key = '48a90ac42caa09f90dcaeee4096b9e53'
+    if request.method == 'POST':
+        city = request.form['city']
+    else:
+        city = 'Kerala'
+    try:
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+api_key).read()
+    except:
+        return abort(404)
+
+    list_of_data = json.loads(source)
+
+    data = {
+        "country_code": str(list_of_data['sys']['country']),
+        "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+        "temp": str(list_of_data['main']['temp']) + 'k',
+        "temp_cel": tocelcius(list_of_data['main']['temp']) + 'C',
+        "pressure": str(list_of_data['main']['pressure']),
+        "humidity": str(list_of_data['main']['humidity']),
+        "cityname":str(city),
+    }
+    return render_template('index.html',data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
